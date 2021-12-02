00f23a3983b401549fe0d5ac040b8a3118a96330
Adityaraj-30 <imadityaraj00@gmail.com>
2021-12-02 19:13:04 +0530

    Added ques8.html

---
+++ prac_ques8/cookies.js
@@ -0,0 +1,34 @@
+//FUNCTION FOR GETTING THE NAME OF LAST USER
+function getCookie(cookieName) {
+    var cookieValue = "";
+    if (document.cookie) {
+        var cookies_string = document.cookie;
+        var cookieArr = cookies_string.split(';');
+        for (var i = 0; i < cookieArr.length; i++) {
+            var cookie = cookieArr[i].trim();
+            if (cookie.startsWith(cookieName)) {
+                var position = cookie.indexOf("=");
+                cookieValue = cookie.slice(position + 1);
+            }
+        }
+    }
+    return cookieValue;
+}
+//FUNCTION FOR SETTING THE COOKIE
+function setCookie(cname, cvalue, exdays) {
+    const day = new Date();
+    day.setTime(day.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + day.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function forgetMe() {
+    delCookie("lastuser");
+}
+var currentUser = prompt("ENTER YOUR NAME : ");
+var lastuser = getCookie("lastuser");
+if (lastuser == currentUser)
+    alert("WELCOME AGAIN " + currentUser)
+else {
+    alert("WELCOME " + currentUser);
+    setCookie("lastuser", currentUser);
+}
